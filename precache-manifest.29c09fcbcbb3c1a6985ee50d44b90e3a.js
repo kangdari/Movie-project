2f5fba4b1e0867dfc039e525f4f561b8c3b3bdb3
kangdari_ <ksh44820@naver.com>
2020-02-14 15:37:37 +0900

    Updates

---
+++ precache-manifest.29c09fcbcbb3c1a6985ee50d44b90e3a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8cf655bf1579f29bcbf943169e4837d",
+    "url": "/movie_project/index.html"
+  },
+  {
+    "revision": "dc0292901bd530f4e32c",
+    "url": "/movie_project/static/css/main.9fb15e86.chunk.css"
+  },
+  {
+    "revision": "ee45472cb06ea6a1d0ae",
+    "url": "/movie_project/static/js/2.33267454.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/movie_project/static/js/2.33267454.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc0292901bd530f4e32c",
+    "url": "/movie_project/static/js/main.d161e090.chunk.js"
+  },
+  {
+    "revision": "2bb5dea3a21f4714b56e",
+    "url": "/movie_project/static/js/runtime-main.2d98bbfd.js"
+  },
+  {
+    "revision": "3e344d255a9c2bc603683f5b078d471d",
+    "url": "/movie_project/static/media/NotoSansKR-Medium.3e344d25.otf"
+  },
+  {
+    "revision": "46818ebd4f76c4e6fe9b030dbc74f5cf",
+    "url": "/movie_project/static/media/Raleway-Black.46818ebd.ttf"
+  },
+  {
+    "revision": "0847227b3c5baa567b6630e28f8cc0d8",
+    "url": "/movie_project/static/media/Sriracha-Regular.0847227b.ttf"
+  }
+]);
